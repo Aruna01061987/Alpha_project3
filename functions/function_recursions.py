6fee8d493c43586f9c407f4d6f78fc97ffee1898
Aruna <arunaprasad.0109@gmail.com>
2022-02-12 08:32:09 +0530

    changes committed on 12-02-2022

--- functions/function_recursions.py
+++ functions/function_recursions.py
@@ -44,18 +44,36 @@
 """Write a recursion program to count """
 #
 # def count_digits(num, count=0):
-#     if num > 0:
+#     # if num > 0:
 #         num = num // 10
 #         count += 1
 #         return count_digits(num, count)
-#     return count
+#     # return count
 #
 #
 # print(count_digits(1234))
 
 """program to reverse a program using recursions"""
 
-def reverse_sting(string, i=0, a = ""):
+# def reverse_sting(string, i=0, a = ""):
+
+
+# def fibonacci(n):
+#     if n < 0:
+#         print("invalid input")
+#     elif n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return (fibonacci(n-1)+ fibonacci(n-2))
+#
+# for i in range(n):
+#     print(fibonacci(i), end= " ")
+
+
+
+
 
 
 
--- functions/lambda_functions.py
+++ functions/lambda_functions.py
@@ -1,15 +1,37 @@
 
+# #
+# # res = lambda str_: str_[-1]
+# #
+# # print(res([1, 2, 3, 4]))
 #
-# res = lambda str_: str_[-1]
+# # res = lambda str_: str_ == str_[::-1]
+# # print(res("madam"))
 #
-# print(res([1, 2, 3, 4]))
-
-# res = lambda str_: str_ == str_[::-1]
-# print(res("madam"))
-
-# even_odd = lambda num: f"{num} is even" if num%2 == 0 else f"{num} is odd"
+# # even_odd = lambda num: f"{num} is even" if num%2 == 0 else f"{num} is odd"
+# #
+# # print(even_odd(13))
 #
-# print(even_odd(13))
+# t = (22, )
 
-t = (22, )
+# list_ = ["hai", "mom", "dad", "hello"]
+# palindrome = lambda string: string if string == string[::-1]
+
+
+# a = map(lambda x: x%2 == 0, [1, 2, 3, 4], [0, 1, 2, 3])
+# print(list(a))
+
+# a = map(lambda x: x % 2 == 0, [1, 2, 3, 4])
+# print(list(a))
+#
+# a = lambda x: x*2
+# print(a((1, 2)))
+
+a = map(lambda x: x % 2 == 0,[1, 2, 3, 4], [0, 1, 2, 3])
+print(list(a))
+
+a = lambda x, y: x*y
+print(list(a(1,2))
+
+
+
 
